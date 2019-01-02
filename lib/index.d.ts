5aaad1c2e57c1f938ddf4e2b4f003b9936e541a9
jelly <zyu1@xiaohongshu.com>
2019-01-02 19:03:40 +0800

    improvement: remove lib from git

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Component as ClassComponent, Emit, Inject, Mixins, Model, Prop, Provide, Watch } from "vue-property-decorator";
-import { Component as TSXComponent } from "vue-tsx-support";
-declare class Component<Props, Events = {}, ScopedSlotArgs = {}> extends TSXComponent<Props, Events, ScopedSlotArgs> {
-    $props: Props;
-}
-export { ClassComponent, Component, Mixins, Prop, Emit, Model, Watch, Provide, Inject, };
